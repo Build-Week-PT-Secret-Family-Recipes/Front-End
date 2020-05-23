6482e41a09b3c526d9ce6c4302e946fea6c1f483
Michael Harris <mhguitar1997@gmail.com>
2020-05-22 17:38:54 -0700

    removed empty files

--- secret-family-recipes/src/App.js
+++ secret-family-recipes/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
-import Login from './components/LoginPage';
 import './App.css';
 import PrivateRoute from './components/PriviteRoute';
 
