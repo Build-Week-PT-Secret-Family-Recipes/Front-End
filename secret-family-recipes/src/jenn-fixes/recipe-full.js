db2bca9601a1e4e5366056f7d36e36eac22d10cd
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-05-29 22:42:26 -0400

    Starting work on recoding of form

--- secret-family-recipes/src/jenn-fixes/recipe-full.js
+++ secret-family-recipes/src/jenn-fixes/recipe-full.js
@@ -1,46 +1,14 @@
 import React from 'react';
-import {
-  Card,
-  CardHeader,
-  CardMedia,
-  CardContent,
-  Typography,
-} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import '../../css/Card.css';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: '23%',
-    margin: '2%',
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-}));
 
 function FullRecipe(props) {
   // const [isOpen, setIsOpen] = useState(false);
   // const toggle = () => setIsOpen(!isOpen);
-  const classes = useStyles();
+  
 
   return (
-    <Card className={classes.root} variant='outlined'>
-      <CardMedia
-        className={classes.media}
-        image='https://via.placeholder.com/150'
-        title={props.recipeData.title}
-      />
-      <CardHeader className='card' title={props.recipeData.title} />
-      <CardContent>
-        <Typography variant='body2' color='textSecondary' component='p'>
-          {props.recipeData.description}
-        </Typography>
-      </CardContent>
-      <p>{props.recipeData.category_name}</p>
-      <button className='view-recipe'>Read Full Recipe</button>
-    </Card>
+    <form>
+      
+    </form>
   );
 }
 export default FullRecipe;
